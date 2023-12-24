4f3c08b57131a4ab85b2908a49b12ae11f36874a
Aleksey Rudikin <helen1969@yandex.ru>
2023-12-24 17:21:18 +0300

    Testing

---
+++ Class1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace vis9
+{
+
+    
+    class MSearch
+    {
+        private int[][] mat;
+        private int maxSum;
+        private int RowIndex;
+
+        public MSearch(int[][] matrix)
+        {
+            this.mat = matrix;
+            maxSum = 0;
+            RowIndex = -1;
+        }
+
+        public void FindMaxSumMatrix()
+        {
+            Thread[] threads = new Thread[mat.Length];
+
+            for (int i = 0; i < mat.Length; i++)
+            {
+                int row = i;
+                threads[i] = new Thread(() => FindMaxSumInRow(row));
+                threads[i].Start();
+            }
+
+            for (int i = 0; i < mat.Length; i++)
+            {
+                threads[i].Join();
+            }
+        }
+
+        private void FindMaxSumInRow(int rowIndex)
+        {
+            int[] row = mat[rowIndex];
+            int sum = 0;
+
+            for (int i = 0; i < row.Length; i++)
+            {
+                sum += row[i];
+            if (sum > maxSum)
+            {
+                lock (this)
+                {
+                    if (sum > maxSum)
+                    {
+                        maxSum = sum;
+                        RowIndex = rowIndex;
+                    }
+                }
+            }
+        }
+        }
+
+        public int GetMaxSum()
+        {
+            return maxSum;
+        }
+        public int GetMaxSumRowIndex()
+        {
+            return RowIndex;
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace vis9
+{
+    public partial class Form1 : Form
+    {
+        string f = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[][] matrix = new int[][]
+                {
+                    new int[] { 100, 2, 3 },
+                    new int[] { 4, 50, 6 },
+                    new int[] { 7, 8, 9 }
+                };
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                for (int j = 0; j < matrix[i].Length; j++)
+                {
+                    textBox1.Text+=matrix[i][j]+"\t";
+                }
+                textBox1.Text +="\r\n";
+            }
+
+
+            MSearch matrixSearch = new MSearch(matrix);
+            matrixSearch.FindMaxSumMatrix();
+            int maxSum = matrixSearch.GetMaxSum();
+            int maxSumRowIndex = matrixSearch.GetMaxSumRowIndex();
+            maxSumRowIndex++;
+            textBox2.Text += "Максимальная сумма: " + maxSum + "\r\n";
+            textBox2.Text += "В строке: "+maxSumRowIndex+"\r\n";
+            
+        }
+        int i = 0;
+        int j = 51;
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            Thread th1 = new Thread(Print1);
+            Thread th2 = new Thread(Print2);
+            th1.Start();
+            th2.Start();
+            
+
+
+            void Print1()
+            {
+                while (i < 5001)
+                {
+                    
+                    if (i % 5 == 0)
+                    {
+                        f += "В строке:1 " + i + " \r\n";
+                        
+                        i++;
+                    }
+                    else i++;
+                }
+                
+            }
+            void Print2()
+            {
+                while (j < 10001)
+                {
+                    if (j % 5 == 0)
+                    {
+                        f += "В строке:2 " + j + " \r\n";
+                        j++;
+                       
+                    }
+                    else j++;
+                }
+
+            }
+            textBox2.Text += f;
+            f = "";
+            i = 0; 
+            j = 50;
+        }
+        static bool flag = true;
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            Thread th1 = new Thread(Print1);
+            Thread th2 = new Thread(Print2);
+            th1.Start();
+            th2.Start();
+            
+
+            void Print1()
+            {
+                while (i <= 51)
+                {
+                    while (!flag) { }
+                    if (i % 5 == 0)
+                    {
+                        f += "В строке:1 " + i + " \r\n";
+                        i++;
+                    }
+                    else i++;
+                    if (i == 51) flag = false;
+                }
+                
+            }
+            void Print2()
+                {
+                    while (j < 1000)
+                    {
+                        while (flag) { }
+                        if (j % 5 == 0)
+                        {
+                            f += "В строке:2 " + j + " \r\n";
+                            j++;
+
+                        }
+                        else j++;
+                        
+                    }
+                }
+            textBox1.Text += f;
+        }
+    }
+}
